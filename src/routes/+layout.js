0d7658639857bdc3db3a4858eb57875c1c833e82
Ahmad Barclay <abarclay.ons@gmail.com>
2022-11-18 18:24:08 +0000

    temp fix for broken places list

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,12 +1,13 @@
 const prerender = false;
 
+import { base } from "$app/paths";
 import { cdnUrl, geoCodesLookup, geoNames } from "$lib/config";
 import { getData, capitalise } from "$lib/utils";
 
 const getTypeLabel = (type) => geoNames[type] ? geoNames[type].label : geoCodesLookup[type].label;
   
 export async function load({ fetch }) {
-	let places = await getData(`${cdnUrl}/places-list.csv`, fetch);
+	let places = await getData(`${base}/data/places.csv`, fetch);
 	places.sort((a, b) => a.areanm.localeCompare(b.areanm));
 	let lookup = {};
 	places.forEach(d => lookup[d.areacd] = d);
