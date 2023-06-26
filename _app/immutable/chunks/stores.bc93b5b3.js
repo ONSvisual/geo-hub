8cddfe86804da1e47860c9ab97ac84e0a3159f06
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2023-06-26 13:32:46 +0100

    Updates

--- _app/immutable/chunks/stores.bc93b5b3.js
+++
@@ -1,1 +0,0 @@
-import"./index.556ea14b.js";import{s as e}from"./singletons.944dbc3a.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
